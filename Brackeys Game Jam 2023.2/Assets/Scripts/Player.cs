f8eb2a7a377fd7df61fe4f22c3a3d19c05a0c92e
CaptainCrookeye <dominiknetri@gmail.com>
2023-08-20 13:39:12 +0200

    basic movement

---
+++ Brackeys Game Jam 2023.2/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    public float fall = 5f;
+    public int health = 5;
+    bool walking = false;
+    bool left = false;
+    bool right = true;
+    void Update()
+    {
+        transform.Translate(Vector2.down * fall * Time.deltaTime);
+        if (walking && right)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else if (walking && left)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            walking = true;
+            right = true;
+            left = false;
+        }
+        if(Input.GetKeyUp(KeyCode.D)) 
+        {
+            walking = false;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            walking = true;
+            right = false;
+            left = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            walking = false;
+        }
+        if (health==0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            health--;
+            Debug.Log(health);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
